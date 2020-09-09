2992e5e9acef7e5cc6393bf9befab9debcc0f624
liuyong_awesome <a3831524@126.com>
2020-09-09 22:20:33 +0800

    增加lru cache

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/list"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/yongliu1992/mmh3"
@@ -10,6 +11,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
 )
 
 type Wz struct {
@@ -22,14 +24,23 @@ var dsn = "?charset=utf8mb4&parseTime=True&loc=Local"
 var db *gorm.DB
 var err error
 var hostName string
+var lru *LruCache
+
+const defaultCap = 100
+
 func main() {
 	cfg, err := ini.Load("app.ini")
 	if err != nil {
 		fmt.Printf("Fail to read file: %v", err)
 		os.Exit(1)
 	}
+	cap, err := cfg.Section("server").Key("cache_nums").Int()
+	if err != nil {
+		cap = defaultCap
+	}
+	lru = NewLruCache(cap)
 	dbs := cfg.Section("db")
-	dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",dbs.Key("db_user").String(),dbs.Key("db_pwd").String(),dbs.Key("db_host").String(),dbs.Key("db_port").String(),dbs.Key("db_name").String())+dsn
+	dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbs.Key("db_user").String(), dbs.Key("db_pwd").String(), dbs.Key("db_host").String(), dbs.Key("db_port").String(), dbs.Key("db_name").String()) + dsn
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
@@ -37,27 +48,105 @@ func main() {
 	r := gin.Default()
 	r.GET("/:name", handlerRedirect)
 	hostName = cfg.Section("server").Key("host").String()
-	log.Fatal(r.Run(":"+cfg.Section("server").Key("port").String()))
+	log.Fatal(r.Run(":" + cfg.Section("server").Key("port").String()))
 }
+
+//加入lru cache
+type LruCache struct {
+	sync.Mutex
+	m   map[string]*list.Element
+	q   *list.List
+	cap int
+}
+
+//最少100条
+func NewLruCache(cap int) *LruCache {
+	if cap < defaultCap {
+		cap = defaultCap
+	}
+	return &LruCache{
+		cap: cap,
+		m:   make(map[string]*list.Element),
+		q:   list.New(),
+	}
+}
+
+type LruCacheEntry struct {
+	k string
+	v *string
+}
+
+func (l *LruCache) Put(key string, v string) {
+	l.Lock()
+	defer l.Unlock()
+	if el, ok := l.m[key]; ok {
+		entry := el.Value.(*LruCacheEntry)
+		entry.v = &v
+		l.q.MoveToFront(el)
+		return
+	}
+	if l.q.Len() < l.cap {
+		l.m[key] = l.q.PushFront(&LruCacheEntry{k: key, v: &v})
+		return
+	}
+	elem := l.q.Back()
+	entry := elem.Value.(*LruCacheEntry)
+	delete(l.m, entry.k)
+	entry.k = key
+	entry.v = &v
+	l.q.MoveToFront(elem)
+	l.m[key] = elem
+	return
+}
+
+func (l *LruCache) Get(key string, moveFront bool) (string, bool) {
+	l.Lock()
+	defer l.Unlock()
+	if elem, ok := l.m[key]; ok {
+		if moveFront {
+			l.q.MoveToFront(elem)
+		}
+		return *elem.Value.(*LruCacheEntry).v, true
+	}
+	return "", false
+}
+
 func handlerNew(c *gin.Context) {
 	ori := c.Query("url")
 	if ori == "" {
 		c.JSON(400, map[string]interface{}{"error": "缺少参数url"})
 		return
 	}
-	m32 := mmh3.Sum32([]byte(ori))
-	sub := from10To64(m32)
-	x := Wz{Sub: sub, Origin: ori}
-	err = db.Create(&x).Error
-	if err != nil {
-		println("err", err)
+	var sub string
+	if cv,ok := lru.Get(ori,true);ok {
+		sub = cv
+	}else {
+		m32 := mmh3.Sum32([]byte(ori))
+		sub = from10To64(m32)
+		x := Wz{Sub: sub, Origin: ori}
+		err = db.Create(&x).Error
+		if err != nil {
+			println("err", err)
+		}
+		lru.Put(ori,sub)
 	}
-	cs := fmt.Sprintf("<a href='%s'>点我</a>",hostName+"/" + sub)
+
+
+	cs := fmt.Sprintf("<a href='%s'>点我</a>", hostName+"/"+sub)
 	if c.Query("format") == "button" {
 		c.Writer.WriteString(cs)
 		return
 	}
-	c.JSON(200,map[string]interface{}{"dwz":hostName+"/" + sub})
+	c.JSON(200, map[string]interface{}{"dwz": hostName + "/" + sub})
+}
+
+func getCache(c *gin.Context) {
+	m := make(map[string]string, len(lru.m))
+	for i, _ := range lru.m {
+		vv, _ := lru.Get(i, false)
+		m[i] = vv
+	}
+	c.JSON(200, m)
 }
 func handlerRedirect(c *gin.Context) {
 	wz := c.Param("name")
@@ -65,8 +154,17 @@ func handlerRedirect(c *gin.Context) {
 		handlerNew(c)
 		return
 	}
-
+	if wz == "c" {
+		getCache(c)
+		return
+	}
 	x := Wz{Sub: wz}
+	if v,ok := lru.Get(wz,true);ok{
+		x.Origin =v
+		c.Redirect(301, x.Origin)
+		return
+	}
+
 	result := db.Where("sub=?", x.Sub).Find(&x)
 	if result.RowsAffected < 1 {
 		c.JSON(400, "not found")
@@ -74,6 +172,9 @@ func handlerRedirect(c *gin.Context) {
 		if !strings.Contains(x.Origin, "http") {
 			x.Origin = "http://" + x.Origin
 		}
+		go func() {
+			lru.Put(wz,x.Origin)
+		}()
 		c.Redirect(301, x.Origin)
 	}
 }
